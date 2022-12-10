c872953491cd91b0c1725660c8d65e263cfaecfd
Evolan <yon_baermund@hotmail.com>
2022-12-09 19:52:05 -0500

    pre-ask BCS push for revision

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,71 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'filename',
+        message: 'What is the name of the Project you are working on?',
+    },
+    {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'What is the purpose of this program?',
+    },
+    {
+        type: 'checkbox',
+        name: 'badges',
+        message: 'Would you like to add any badges to the md file?',
+        choices: ['JavaScript', 'nodeJs'],
+    },
+    {
+        type: 'input',
+        name: 'initGuide',
+        message: 'can you describe how to install this program?',
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'please choose the License badge(s) you would like to add.',
+        choices: ['MIT License', 'CC Zero License', 'Unilicense'],
+        //options {
+        //if (chosen == true) {
+        // renderLicenseSection();
+    },
+    {
+        type: 'input',
+        name: 'programTest',
+        message: 'please provide a small description on how to use this program:',
+    },
+    {
+        type: 'input',
+        name: 'emailInfo',
+        message: 'Enter your email for contact:',
+        },
+    {
+        type: 'input',
+        name: 'githubInfo',
+        message: 'Please provide your Github username: ',
+    },
+    ])
+    .then((data) => {
+        writeToFile(data);
+})};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(filename, data) {
+    fs.writeFile(filename, generateMarkdown(data), (err) =>
+    err ? console.error(err) : console.log('Readme file created successfuly!')
+    );}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions();
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -10,11 +10,53 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// TODO: Create a function to generate markdown for README. THIS IS WHERE WE CREATE THE MARKDOWN TEMPLATE
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectName}
 
-`;
+## Description
+
+- ${data.description}
+
+## Technical Skills
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+
+## Table of Contents 
+
+- [Description](#description)
+- [Instalation](#instalation)
+- [License](#license)
+- [Test](#test)
+- [Contact](#contact)
+- [How to Contribute](#how-to-contribute)
+
+
+
+## License
+
+[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+
+Copyright (c) [2022] [Yonatan Yael Bermudez Diez]
+
+
+## Features
+
+- 
+- 
+
+## How to Contribute
+
+Constructive Comments are always welcome. You can e-mail me ${emailInfo}. Thank you for checking out our website.`
+
 }
+
 
 module.exports = generateMarkdown;
